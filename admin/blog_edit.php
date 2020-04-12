903d429cfb3642f3f13a5d096670ce368e3abdc7
zahid <mdzahidhasan3616@gmail.com>
2020-04-12 09:43:08 +0600

    edite page cover successfully

--- admin/blog_edit.php
+++ admin/blog_edit.php
@@ -1,10 +1,20 @@
 <?php
-
+//show information
 $blog_id=$_GET['id'];
     require_once '../class/blog_class.php';
     $show_for=new blog();
     $query_result=$show_for->show_for_edit($blog_id);
     $final_query_result=mysqli_fetch_assoc($query_result);
+
+    //update Information
+    $massage="";
+    if (isset($_POST['zahid'])) {
+      require_once '../class/blog_class.php';
+      $update=new blog();
+      $massage=$update->update_blog_info($_POST);
+    }
+
+
  ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -28,12 +38,14 @@ $blog_id=$_GET['id'];
   <section class="p-5 bg bg-success m-5">
     <div class="container">
       <div class="row">
+        <h3><?php echo $massage; ?></h3>
         <div class="col-md-12 col-lg-12">
 
           <form action="" method="post" enctype="multipart/form-data">
             <div class="form-group">
               <label for="exampleInputEmail1">Author Name </label>
               <input type="text" name="author_name" value="<?php echo $final_query_result['author_name']; ?>" class="form-control">
+              <input type="hidden" name="blog_id" value="<?php echo $final_query_result['blog_id']; ?>" class="form-control">
             </div>
             <div class="form-group">
               <label for="exampleInputEmail1">Blog Title </label>
--- class/blog_class.php
+++ class/blog_class.php
@@ -79,8 +79,52 @@ public function show_for_edit($data){
     $query_result=mysqli_query($this->db_conn,$sql);
     return $query_result;
   }
-
 }
+
+
+
+
+
+// update infomation on database
+public function update_blog_info($data){
+  $tem_img= $_FILES['blog_image']['tmp_name'];
+  $img_name=$_FILES['blog_image']['name'];
+  $derectory="blog_image/";
+  $image_url=$derectory.$img_name;
+  $image_size=$_FILES['blog_image']['size'];
+  $image_type=pathinfo($img_name .PATHINFO_EXTENSION); //type chack
+  $img_re_type=$image_type['extension'];
+  $check=getimagesize($tem_img);   // check image
+  if ($check) {
+    if (file_exists($image_url)) {
+      die("Image All ready Exists");
+    }else {
+      if ($image_size < 500000) {
+        if ($img_re_type='jpg4' && $img_re_type= 'png') {
+          move_uploaded_file($tem_img,$image_url);
+          $sql="UPDATE add_blog_info SET author_name='$data[author_name]', blog_title='$data[blog_title]', blog_description='$data[blog_description]', blog_status='$data[blog_status]', blog_image='$image_url' WHERE blog_id='$data[blog_id]'";
+          if (mysqli_query($this->db_conn,$sql)) {
+            $massage= "Blog Information Update Successfully";
+            return $massage;
+          }else {
+            die("Query Porblem" .mysqli_connect_error($this->db_conn));
+          }
+        }else {
+          die("Your file type is not jpg or png");
+        }
+      }else {
+        die("Your File Too Large");
+      }
+    }
+  }else {
+    die("Your uploaded file is not an image please upload a image");
+  }
+}
+
+
+
+
+
 
 }
  ?>
